2da23bb7df2b4a0f575d7270a2812a070ab62d48
David Baumgold <david@davidbaumgold.com>
2018-06-13 10:50:47 +0200

    Add info to resolver interface

--- src/interface/ResolverInterface.ts
+++ src/interface/ResolverInterface.ts
@@ -1,6 +1,8 @@
+import {GraphQLResolveInfo} from "graphql";
+
 /**
  * Resolvers can implement this type to provide a proper method signatures for some model T.
  */
 export type ResolverInterface<T> = {
-    [P in keyof T]?: (entities: T|T[], args?: object, context?: any) => Promise<(T[P][])|T[P]>|((T[P][])|T[P]);
+    [P in keyof T]?: (entities: T|T[], args?: object, context?: any, info?: GraphQLResolveInfo) => Promise<(T[P][])|T[P]>|((T[P][])|T[P]);
 };
